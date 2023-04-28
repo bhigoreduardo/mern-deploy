bd1d48098cbda3715bbba7fa88d9d8aba51481cd
bhigoreduardo <bhigoreduardo@gmail.com>
2023-04-28 11:30:13 -0300

    :tada: Create server app

---
+++ server/src/routes/users.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+
+const userController = require('../controllers/user');
+const auth = require('../middlewares/auth');
+
+router.post('/sign-up', userController.signUp);
+router.post('/login', userController.login);
+router.get('/list', auth, userController.getAllUsers);
+module.exports = router;
+// module.exports = (app) => app.use('/users', router);
